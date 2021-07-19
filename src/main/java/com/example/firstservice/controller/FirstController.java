bc789f32db829d4b3f930b72a2d9f631cc1647be
zzangisdaeho <zzangisdaeho@naver.com>
2021-07-19 23:37:23 +0900

    21-07-19

--- src/main/java/com/example/firstservice/controller/FirstController.java
+++ src/main/java/com/example/firstservice/controller/FirstController.java
@@ -1,13 +1,37 @@
 package com.example.firstservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class FirstController {
 
-    @GetMapping("/hello")
+    private final Environment env;
+
+    @GetMapping("/welcome")
     public String hello(){
         return "first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        System.out.println(header);
+        log.info(header);
+        return "Hello in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}", request.getServerPort());
+        return String.format("first check %s"
+                , env.getProperty("local.server.port"));
     }
 }
