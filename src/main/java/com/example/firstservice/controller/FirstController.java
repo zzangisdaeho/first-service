e3630e49f390a99a5d3e4ce9411b15006367f622
daeho kim <daeho.kim@softwareinlife.com>
2021-07-20 15:18:04 +0900

    scope

--- src/main/java/com/example/firstservice/controller/FirstController.java
+++ src/main/java/com/example/firstservice/controller/FirstController.java
@@ -2,9 +2,12 @@ package com.example.firstservice.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+//@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class FirstController {
 
     private final Environment env;
+
+//    private String testname = null;
 
     @GetMapping("/welcome")
     public String hello(){
@@ -34,4 +40,15 @@ public class FirstController {
         return String.format("first check %s"
                 , env.getProperty("local.server.port"));
     }
+
+//    @GetMapping("/testread")
+//    public String testread(){
+//        return testname;
+//    }
+//
+//    @GetMapping("/testwrite")
+//    public String testwrite(@RequestParam("name") String name){
+//        this.testname = name;
+//        return testname;
+//    }
 }
